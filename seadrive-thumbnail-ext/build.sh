ea1602f8b4bcfb8028f05ed73f316da83a51a48f
Shuai Lin <linshuai2012@gmail.com>
2019-12-13 17:11:23 +0800

    Fixed seadrive ext build script

--- seadrive-thumbnail-ext/build.sh
+++ seadrive-thumbnail-ext/build.sh
@@ -2,6 +2,11 @@
 
 set -e -x
 
+SCRIPT=$(readlink -f "$0")
+SRCDIR=$(dirname "${SCRIPT}")
+
+cd $SRCDIR
+
 if ! [[ -f CMakeCache.txt ]]; then
     # TODO: compile both 64bit and 32bit DLL
     cmake -G "Visual Studio 14 2015 Win64" .
@@ -10,4 +15,4 @@ fi
 # /nr:false is to prevent msbuild not exiting
 # See https://stackoverflow.com/a/3919906/1467959
 export MSBUILDDISABLENODEREUSE=1
-msbuild -nr:false seadrive_thumbnail_ext.vcxproj
+msbuild -nr:false seadrive_ext.vcxproj
