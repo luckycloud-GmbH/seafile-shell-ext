e3275fe480aef3cb800e0455aa4a1ed1e2f12625
Shuai Lin <linshuai2012@gmail.com>
2019-12-13 17:47:18 +0800

    Build both 32 & 64 bit seadrive ext dll

--- seadrive-thumbnail-ext/build.sh
+++ seadrive-thumbnail-ext/build.sh
@@ -7,12 +7,27 @@ SRCDIR=$(dirname "${SCRIPT}")
 
 cd $SRCDIR
 
-if ! [[ -f CMakeCache.txt ]]; then
-    # TODO: compile both 64bit and 32bit DLL
-    cmake -G "Visual Studio 14 2015 Win64" .
-fi
+build_one() {
+    local is64bit=$1
+    if [[ $is64bit == "true" ]]; then
+        generator="Visual Studio 14 2015 Win64"
+        target=seadrive_ext64.dll
+    else
+        generator="Visual Studio 14 2015"
+        target=seadrive_ext.dll
+    fi
 
-# /nr:false is to prevent msbuild not exiting
-# See https://stackoverflow.com/a/3919906/1467959
-export MSBUILDDISABLENODEREUSE=1
-msbuild -nr:false seadrive_ext.vcxproj
+    rm -f CMakeCache.txt
+    cmake -G "$generator" .
+
+    # /nr:false is to prevent msbuild not exiting
+    # See https://stackoverflow.com/a/3919906/1467959
+    export MSBUILDDISABLENODEREUSE=1
+    msbuild -nr:false seadrive_ext.vcxproj
+
+    mkdir -p lib/
+    cp Debug/seadrive_ext.dll lib/$target
+}
+
+build_one "true"
+build_one "false"
